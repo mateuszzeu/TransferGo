71eb2699f22bd7f8d15716456d4a5e1bb603e3c0
Mat <mateuszzeu@gmail.com>
2025-09-23 15:52:26 +0200

    Initial Commit

---
+++ TransferGo/TransferGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferGoApp.swift
+//  TransferGo
+//
+//  Created by MAT on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TransferGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
