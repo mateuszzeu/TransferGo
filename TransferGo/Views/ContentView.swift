087bc036b2457a52271940c3db9267ab4aba03e2
Mat <mateuszzeu@gmail.com>
2025-09-23 17:23:33 +0200

    feat: Add network layer for TransferGo API integration

---
+++ TransferGo/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TransferGo
+//
+//  Created by MAT on 23/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Test")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
